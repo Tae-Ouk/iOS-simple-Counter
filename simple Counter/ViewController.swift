4c8ed43d3b0a94d1c152de716ac8c1132fbe5ba9
Tae-Ouk <to99820@naver.com>
2024-03-19 20:22:23 +0900

    simple Counter App

--- simple Counter/ViewController.swift
+++ simple Counter/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var CounterLabel: UILabel!
+    
+    private var countData: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resetDisplay()
     }
 
+    @IBAction func counterAdd(_ sender: Any) {
+        self.countData += 1
+        resetDisplay()
+    }
+    
+    @IBAction func counterSub(_ sender: Any) {        self.countData -= 1
+        resetDisplay()
+    }
+    
+    func resetDisplay() {
+        self.CounterLabel.text = "\(self.countData)"
+    }
 
 }
-
